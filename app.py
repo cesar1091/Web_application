d3c8efd1cae9c93e3f5c167521f1b5198ad9efba
Cesar_Fernandez <cafn.1091@gmail.com>
2021-03-26 11:43:49 -0500

    Agregamos quincena app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import vsigv_semanal, vsigv_mes, home
+from apps import vsigv_semanal, vsigv_quincena, vsigv_mes, home
 
 # import your app modules here
 app = MultiApp()
@@ -13,6 +13,7 @@ Select the time of prediccion
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Semanal", vsigv_semanal.app)
+app.add_app("Quincena", vsigv_quincena.app)
 app.add_app("Mensual", vsigv_mes.app)
 
 # The main app
